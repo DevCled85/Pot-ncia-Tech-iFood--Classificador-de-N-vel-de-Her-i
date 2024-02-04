d4d9b400fbcf6853c6bda8e25f6d13c1f0991b44
Cledson do Nascimento Pereira <142537029+DevCled85@users.noreply.github.com>
2024-02-04 09:03:50 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// Variáveis para armazenar o nome e a quantidade de XP do herói
+let nomeHeroi = "Javascript";
+let xpHeroi = 10000;
+let nivel;
+
+// Utilizando estrutura de decisão para determinar o nível
+if (xpHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+// Exibindo a mensagem final
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}.`);
